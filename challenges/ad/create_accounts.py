8a95e8df48fedd2a205ab730158b562d6a0d73a7
Chris Bradski <chris.bradski@giant.ai>
2023-06-28 13:00:17 -0700

    log accounts, attempt ad

--- challenges/ad/create_accounts.py
+++ challenges/ad/create_accounts.py
@@ -1,18 +1,22 @@
 from posixpath import split
+
+import csv
+from tkinter.ttk import Separator
 import names
 import os
 import random
 import string
 
-#from pyad import *
+from pyad import *
 
 AD_ADMINISTRATOR=os.environ.get('AD_ADMINISTRATOR','Administrator')
 AD_ADMINISTRATOR_PASS=os.environ.get('AD_ADMINISTRATOR_PASS', 'from the lab')
 
-#p = pyad.set_defaults(ldap_server='', username=AD_ADMINISTRATOR, password=AD_ADMINISTRATOR_PASS)
+p = pyad.set_defaults(ldap_server='', username=AD_ADMINISTRATOR, password=AD_ADMINISTRATOR_PASS)
 
 unique_names = {}
 sam_length = 7
+header_list = ['sam', 'FullName', 'FirstName', 'LastName', 'Password']
 
 words_path = '4_letter_words.txt'
 
@@ -24,14 +28,32 @@ def gen_random_password(words_file_path):
     random_words = random.sample(words, 3)
     return '-'.join(random_words) + str(random.randrange(1,8))
 
-while len(unique_names) < 501:
-    user = names.get_full_name()
-    sam  = random.choice(string.ascii_lowercase)
-    sam += ''.join(random.choices(string.ascii_lowercase + string.digits, k=sam_length))
-    unique_names[sam] ={
-        'FullName': user,
-        'First': user.split(' ')[0],
-        'Last': user.split(' ')[1],
-        'Password': gen_random_password(words_path)
-    }
-    print(sam, unique_names[sam])
+def create_user_in_ad(data):
+    ou = pyad.adcontainer.ADContainer.from_dn("ou=All_Users, dc=hackerrank, dc=net")
+    new_user = pyad.aduser.ADUser.create(data['FirstName'],
+        ou,
+        data['Password'],
+        optional_attributes={
+            "displayName": data['FullName'],
+            "sam": data['sam'],
+            "sn": data['LastName']
+        }
+    
+    )
+
+with open('created_users.csv', 'w') as output:
+    w = csv.DictWriter(output, fieldnames=header_list)
+    w.writeheader()
+    while len(unique_names) < 501:
+        user = names.get_full_name()
+        sam  = random.choice(string.ascii_lowercase)
+        sam += ''.join(random.choices(string.ascii_lowercase + string.digits, k=sam_length))
+        unique_names[sam] ={
+            'sam': sam,
+            'FullName': user,
+            'FirstName': user.split(' ')[0],
+            'LastName': user.split(' ')[1],
+            'Password': gen_random_password(words_path)
+        }
+        print(sam, unique_names[sam])
+        w.writerow(unique_names[sam])
